d4a13cb5419f2e420378a7258c2289e29137689f
roberttarkanyi1988-alt <robert.tarkanyi1988@gmail.com>
2026-05-11 22:51:04 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'ea-strategies-v1';
+const URLS_TO_CACHE = [
+  '/landing',
+  '/client',
+  '/manifest.json',
+  '/icon-512.svg',
+  '/icon-192.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request)
+    )
+  );
+});
